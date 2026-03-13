06b2e0baae7114c521be9c88651f71f3b1ef79c2
Ale <alexandra.mita@student.upt.ro>
2026-03-14 01:47:00 +0200

    feat: add initial Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ export default function Home() {
     <div className="min-h-screen bg-white">
       <Navigation />
       <Hero />
+      <div className="p-8 bg-red-500 text-white text-center">
+        <h2 className="text-2xl font-bold">Test Section - If you see this, Tailwind is working!</h2>
+      </div>
     </div>
   )
 }
